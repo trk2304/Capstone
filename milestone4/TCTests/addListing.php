c00dd9e2a6f5508b13d77e00c31fa97c478f1922
Titus Kilpatrick <m213438@usna.edu>
2020-10-28 21:24:30 -0400

    Struggling on addListing.php script

--- milestone4/TCTests/addListing.php
+++ milestone4/TCTests/addListing.php
@@ -5,7 +5,7 @@
     TO GET INFO FROM A TABLE.
 */
 
-// Create a new DB 
+// Create a new connection to the database.
 $conn = new mysqli('midn.cs.usna.edu', 'm213438', 'changeme', 'midtrade');
 
 // Check if connection was successful
@@ -21,5 +21,45 @@ if ($conn->connect_error) {
  * 3. Assuming that the username exist, generate a prepared statement with the username, userID, asking price, item description, and item name and INSERT into the table.
  * 4. Report success.
  */
+
+// Begin step 1. Check if the POST variable is set.
+if(isset($_POST)) {
+
+// Step 2: Collect the userID from the user table based on the username. userID is bound in $userID variable.
+$stmt = $conn->prepare("SELECT userID FROM listings WHERE username = ?");
+
+$username = $_POST['add_listing_username'];
+htmlspecialchars($username);
+$username = strip_tags($username);
+
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$stmt->bind_result($userID);
+$stmt->fetch();
+
+if(gettype($userID) != "integer") {
+    die("The username does not exist");
+}
+
+// Step 2.1: Scrub the input given by the user.
+// First, let's get the user's input
+$itemName = htmlspecialchars($_POST['add_listing_itemname']);
+
+$askingprice = $_POST['add_listing_price'];
+
+$description = htmlspecialchars($_POST['add_listing_description']);
+
+echo $username . " " . $userID . " " . $itemName . " " . $askingprice . " " . $description;
+
+// Step 3: Generate a prepared statment to insert the information you've scrubbed.
+$sql = "INSERT INTO listings (userID, username, ItemName, AskingPrice, ItemDescription) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('issds', $userID, $username, $itemName, $askingprice, $description);
+$stmt->execute();
+
+
+} else {
+    echo 'Nothing submitted';
+}
 
 ?>
