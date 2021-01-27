6161a67be5e154765666bb392e032e89c2680943
tomsavarese <71273125+tomsavarese@users.noreply.github.com>
2021-01-27 13:11:29 -0500

    Add files via upload

---
+++ vue/vue-work/src_tom/components/tel-input.vue
@@ -0,0 +1,16 @@
+// src/components/tel-input.vue
+
+<template>
+    <vue-tel-input v-model="value"></vue-tel-input>
+</template>
+
+<script>
+import { VueTelInput } from 'vue-tel-input'
+import { abstractField } from 'vue-form-generator'
+
+export default {
+  name: 'TelephoneInput',
+  mixins: [abstractField],
+  components: {VueTelInput}
+}
+</script>
---
+++ vue/vue-work/src_tom/router/index.js
@@ -0,0 +1,29 @@
+// src/router/index.js
+
+import Vue from 'vue'
+import Router from 'vue-router'
+import Users from '../pages/Users'
+import CreateUser from '../pages/CreateUser'
+import EditUser from '../pages/EditUser'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'Users',
+            component: Users
+        },
+        {
+            path: '/create',
+            name: 'CreateUser',
+            component: CreateUser
+        },
+        {
+            path: '/edit/:id',
+            name: 'EditUser',
+            component: EditUser
+        }
+    ]
+})
